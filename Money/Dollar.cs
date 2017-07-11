26543ae8f98c2af00058b8d2f68c43f0f3ec9332
Tim Hacker <tim.hacker@trueclarity.co.uk>
2017-07-11 14:53:48 +0100

    Now compiling, but test failing

---
+++ Money/Dollar.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Money
+{
+    public class Dollar
+    {
+        public Dollar(int amount)
+        {
+
+        }
+
+        public int Amount;
+
+        public void Times(int multiplier)
+        {
+
+        }
+    }
+}
