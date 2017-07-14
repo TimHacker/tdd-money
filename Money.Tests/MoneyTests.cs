7dcab5184ea02c125a95c7138a9600187168504e
Tim Hacker <tim.hacker@trueclarity.co.uk>
2017-07-14 16:32:26 +0100

    We can now inline the product variable as it doesn't add much

--- Money.Tests/MoneyTests.cs
+++ Money.Tests/MoneyTests.cs
@@ -10,19 +10,16 @@ namespace Money.Tests
         {
             Dollar five = new Dollar(5);
 
-            Dollar product = five.Times(2);
+            Assert.Equal(new Dollar(10), five.Times(2));
 
-            Assert.Equal(new Dollar(10), product);
-            
-            product = five.Times(3);
-
-            Assert.Equal(new Dollar(15), product);
+            Assert.Equal(new Dollar(15), five.Times(3));
         }
 
         [Fact]
         public void TestEquality()
         {
             Assert.Equal(new Dollar(5), new Dollar(5));
+            
             Assert.NotEqual(new Dollar(5), new Dollar(6));
         }
     }
