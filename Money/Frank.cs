e3f9fd356b0b0576a5865fc9d66645d644886261
Tim Hacker <tim.hacker@trueclarity.co.uk>
2017-07-17 16:58:26 +0100

    Now we can remove duplication from Frank
    I had to make the _amount public while I refactored the Equals in Frank and Dollar before moving them to Money superclass. It must be a difference betweet .NET and Java

--- Money/Frank.cs
+++ Money/Frank.cs
@@ -2,26 +2,16 @@ using System;
 
 namespace Money
 {
-    public class Frank
+    public class Frank : Money
     {
         public Frank(int amount)
         {
             _amount = amount;
         }
 
-        private int _amount;
-
         public Frank Times(int multiplier)
         {
             return new Frank(_amount * multiplier);
-        }
-
-        // override object.Equals
-        public override bool Equals (object obj)
-        {
-            Frank frank = (Frank)obj;
-
-            return _amount == frank._amount;
         }
     }
 }
